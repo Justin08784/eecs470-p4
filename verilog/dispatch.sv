024273d8314771077246fa825ff43a9db4307cfd
Ethan R Sanders <ethansan@caen-vnc-mi15.engin.umich.edu>
2025-02-22 15:41:27 -0500

    Implemented dispatch.sv ... I think (Feel free to look it over to check my work)

--- verilog/dispatch.sv
+++ verilog/dispatch.sv
@@ -1,15 +1,38 @@
 `include "sys_defs.svh"
 
+// `define fetch_cnt 19
 
 module dispatch (
     input clock,
     input reset,
     input [$clog(RS_SZ):0] rs_free,
     input [$clog(ROB_SZ):0] rob_free,
-    output [31:0] insts [N:0],
-    output [N:0] inst_valid
+    output [$clog(`N):0] dispatch_cnt, //tells stage_if how many insts to dispatch
+    output dispatch_vld //will be used to set if_valid to false if cnt == 0
 );
 
+// logic next_cnt;
+logic next_vld;
 
+// I think we actually just want this in an always_comb
+// and not clock dependent bc if it is clock dependent
+// the net output would end up being a cycle behind
+always_comb begin
+    assign dispatch_cnt = (rs_free <= rob_free) ? rs_free : rob_free;
+    assign dispatch_vld = dispatch_cnt > 0;
+end
+
+
+
+// always_ff(@posedge clock) begin
+//     if (reset) begin
+//         dispatch_cnt <= 0;
+//         dispatch_vld <= 0;
+//     end
+//     else begin
+//         dispatch_cnt <= next_cnt;
+//         dispatch_vld <= next_vld;
+//     end
+// end
 
 endmodule
