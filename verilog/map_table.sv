6269a84c7297a2e34b73c9094720970b644007c2
Ethan R Sanders <ethansan@caen-vnc-mi18.engin.umich.edu>
2025-02-24 20:32:38 -0500

    forgot to add the map_table.sv file to last commit

---
+++ verilog/map_table.sv
@@ -0,0 +1,50 @@
+`include "sys_defs.svh"
+
+
+module map_table(
+    input clock,
+    input reset,
+
+    input new_map,
+
+    output map_table
+);
+
+always_ff @(posedge clock) begin
+    if (reset) begin
+        map_table <= default_map_table();
+    end
+    else begin
+        map_table <= new_map;
+    end    
+end
+
+endmodule
+
+
+
+module arch_map(
+
+);
+
+
+endmodule
+
+
+module free_list(
+
+);
+
+
+
+endmodule
+
+
+task default_map_table;
+
+    output default_table;
+
+    begin
+
+    end
+endtask
