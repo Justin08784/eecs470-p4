e9a5d93e06ab6349861579a9eb82e6c360659488
Justin Shin <shine777justin@gmail.com>
2025-03-06 19:02:30 -0500

    fifo testing skele

--- test/fifo_test.sv
+++ test/fifo_test.sv
@@ -44,6 +44,14 @@ module fifo_test();
     
     // Variable to count values written to FIFO
     int cnt;
+
+    always begin
+        #(`CLOCK_PERIOD/2) clock = ~clock;
+    end
+    // Generate random numbers for our write data on each cycle
+    always @(negedge clock) begin
+        std::randomize(wr_data);
+    end
     
     // FIFO instance
     fifo #(
@@ -62,6 +70,185 @@ module fifo_test();
         .free_scnt  (free_scnt),
         .used_scnt  (used_scnt)
     );
+
+    initial begin
+        $display("\nStart Testbench");
+        clock = 0;
+        reset = 1;
+        wr_en_cnt = 0;
+        rd_en_cnt = 0;
+
+        $monitor("  %3d | d_in: %b   wr_en_cnt: %d  rd_en_cnt: %d  |  d_out: %b   used_scnt: %2d  free_scnt: %2d",
+            $time,
+            wr_data,
+            wr_en_cnt,
+            rd_en_cnt,
+            rd_data, 
+            used_scnt, 
+            free_scnt);
+
+        @(negedge clock);
+        @(negedge clock);
+        reset = 0;
+
+        // ---------- Test 1 ---------- //
+        $display("\nTest 1: invalid read");
+        rd_en_cnt = 2'b01;
+        @(negedge clock);
+        rd_en_cnt = 2'b00;
+
+        // // ---------- Test 2 ---------- //
+        // $display("\nTest 2: Write and read with one cycle wait");
+        // $display("Write 1 value");
+        // wr_en_cnt = 2'b01;
+        // @(negedge clock);
+        // wr_en_cnt = 2'b00;
+
+        // $display("Wait one cycle");
+        // @(negedge clock);
+
+        // rd_en_cnt = 2'b01;
+        // $display("Read 1 value");
+        // @(negedge clock);
+        // rd_en_cnt = 2'b00;
+
+        // // ---------- Test 2.5 ---------- //
+        // $display("\nTest 2.5: Write and read twice with one cycle wait");
+        // $display("Write 1 value");
+        // wr_en_cnt = 2'b11;
+        // @(negedge clock);
+        // wr_en_cnt = 2'b00;
+
+        // $display("Wait one cycle");
+        // @(negedge clock);
+
+        // rd_en_cnt = 2'b11;
+        // $display("Read 1 value");
+        // @(negedge clock);
+        // rd_en_cnt = 2'b00;
+
+        // // // ---------- Test 3 ---------- //
+        // $display("\nTest 3: Write and read with no wait");
+        // $display("Write 1 value");
+        // wr_en_cnt = 2'b01;
+        // @(negedge clock);
+        // wr_en_cnt = 2'b00;
+
+        // rd_en_cnt = 2'b01;
+        // $display("Read 1 value");
+        // @(negedge clock);
+        // rd_en_cnt = 2'b00;
+
+        // // // ---------- Test 3.5 ---------- //
+        // $display("\nTest 3.5: Write and read twice with no wait");
+        // $display("Write 1 value");
+        // wr_en_cnt = 2'b11;
+        // @(negedge clock);
+        // wr_en_cnt = 2'b00;
+
+        // rd_en_cnt = 2'b11;
+        // $display("Read 1 value");
+        // @(negedge clock);
+        // rd_en_cnt = 2'b00;
+
+        // // // ---------- Test 4 ---------- //
+        // $display("\nTest 4: Read and write when empty");
+        // wr_en_cnt = 2'b11;
+        // rd_en_cnt = 2'b11;
+        // @(negedge clock);
+        // rd_en_cnt = 2'b00;
+
+        // // // ---------- Test 5 ---------- //
+        // $display("\nTest 5: Write 6 values");
+        // wr_en_cnt = 2'b11;
+        // repeat (2) @(negedge clock);
+        // wr_en_cnt = 2'b10;
+        // @(negedge clock);
+        // wr_en_cnt = 2'b01;
+        // @(negedge clock);
+        // wr_en_cnt = 2'b00;
+
+        // // // ---------- Test 6 ---------- //
+        // $display("\nTest 6: Read 4 values");
+        // rd_en_cnt = 2'b11;
+        // @(negedge clock);
+        // rd_en_cnt = 2'b10;
+        // @(negedge clock);
+        // rd_en_cnt = 2'b01;
+        // @(negedge clock);
+        // rd_en_cnt = 2'b00;
+
+        // // // ---------- Test 7 ---------- //
+        // $display("\nTest 7: Write until full");
+        // cnt = 2;
+        // wr_en_cnt = 2'b11;
+        // while (!full) begin
+        //     cnt += 2;
+        //     @(negedge clock);
+        // end
+
+        // // // ---------- Test 8 ---------- //
+        // $display("\nTest 8: Invalid write");
+        // wr_en_cnt = 2'b11;
+        // @(negedge clock);
+
+        // // // ---------- Test 9 ---------- //
+        // $display("\nTest 9: Simultaneous read and write when full");
+        // rd_en_cnt = 2'b11;
+        // @(negedge clock);
+        // wr_en_cnt = 2'b00;
+        // rd_en_cnt = 2'b00;
+        // @(negedge clock);
+
+        // // // ---------- Test 10 ---------- //
+        // $display("\nTest 10: Write when one less than full");
+        // rd_en_cnt = 2'b01;
+        // $display("Read one");
+        // @(negedge clock);
+        // $display("Write two");
+        // wr_en_cnt = 2'b11;
+        // rd_en_cnt = 2'b00;
+        // @(negedge clock);
+        // $display("Read another one");
+        // rd_en_cnt = 2'b10;
+        // @(negedge clock);
+        // wr_en_cnt = 2'b00;
+        // rd_en_cnt = 2'b00;
+        // @(negedge clock);
+
+        // // // ---------- Test 11 ---------- //
+        // $display("\nTest 11: Read all values");
+        // rd_en_cnt = 2'b11;
+        // while (cnt > 0) begin
+        //     cnt -= 2;
+        //     @(negedge clock);
+        // end
+
+        // // ---------- Test 12 ---------- //
+        // $display("\nTest 12: Invalid read");
+        // rd_en_cnt = 2'b11;
+        // @(negedge clock);
+        // rd_en_cnt = 2'b00;
+
+        // // ---------- Test 13 ---------- //
+        // $display("\nTest 13: Four simultaneous reads and writes");
+        // rd_en_cnt = 2'b10;
+        // wr_en_cnt = 2'b01;
+        // repeat (4) @(negedge clock);
+        // wr_en_cnt = 2'b00;
+
+        // // ---------- Test 14 ---------- //
+        // $display("\nTest 14: Read last item");
+        // @(negedge clock);
+        // rd_en_cnt = 2'b00;
+
+        // @(negedge clock);
+        // @(negedge clock);
+
+        // $display("\n\033[32m@@@ Passed\033[0m\n");
+
+        $finish;
+    end
 
 
 endmodule
