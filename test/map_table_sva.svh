0eb56b6d22e6c2a251a85a6e81c2b63440baef68
Justin Shin <shine777justin@gmail.com>
2025-03-17 18:09:31 -0400

    correct_dep: make it per tag1 or tag2: check_sources

--- test/map_table_sva.svh
+++ test/map_table_sva.svh
@@ -148,44 +148,31 @@ module mt_sva #(parameter
         end
     endtask
 
-    function automatic logic compute_expected_src_tag(
-        input int i,            // which dispatch slot
-        input PHYS_REG_IDX t1,
-        input PHYS_REG_IDX t2
-    );
-        REG_IDX src1, src2;
-        PHYS_REG_IDX true_t1, true_t2;
-        logic from_state1, from_state2;
+    function automatic logic check_sources(
+         input int i,            // which dispatch slot
+         input PHYS_REG_IDX t1,
+         input PHYS_REG_IDX t2
+     );
+         REG_IDX src1;
+         PHYS_REG_IDX true_t1;
+         logic from_state1;
+ 
+         src1 = ins_cur.d_in.src1s[i];
+ 
+         from_state1 = `TRUE;
+         for (int j = i - 1; j >= 0; --j) begin
+             if (ins_cur.d_in.dsts[j] == src1) begin
+                 from_state1 = `FALSE;
+                 true_t1 = ins_cur.d_in.ts[j];
+                 break;
+             end
+         end
+         if (from_state1)
+             true_t1 = entries_pre[src1].t;
+ 
+         return (t1 == true_t1);
+     endfunction
 
-        src1 = ins_cur.d_in.src1s[i];
-        src2 = ins_cur.d_in.src2s[i];
-
-        from_state1 = `TRUE;
-        for (int j = i - 1; j >= 0; --j) begin
-            if (ins_cur.d_in.dsts[j] == src1) begin
-                from_state1 = `FALSE;
-                true_t1 = ins_cur.d_in.ts[j];
-                break;
-            end
-        end
-        if (from_state1)
-            true_t1 = entries_pre[src1].t;
-
-        from_state2 = `TRUE;
-        for (int j = i - 1; j >= 0; --j) begin
-            if (ins_cur.d_in.dsts[j] == src2) begin
-                from_state2 = `FALSE;
-                true_t2 = ins_cur.d_in.ts[j];
-                break;
-            end
-        end
-        if (from_state2)
-            true_t2 = entries_pre[src2].t;
-
-        $display("%d %d %d %d", t1, true_t1, t2, true_t2);
-
-        return (t1 == true_t1 && t2 == true_t2);
-    endfunction
 
     clocking cb @(posedge clock);
         property zero_reg_invariant;
@@ -195,10 +182,10 @@ module mt_sva #(parameter
 
         property correct_deps(i);
             disable iff (reset)
-            (ins_cur.d_in.en_cnt > i) |-> (compute_expected_src_tag(
+            (ins_cur.d_in.en_cnt > i) |->  (check_sources(
                 i,
                 outs_cur.d_out.t1s[i],
-                outs_cur.d_out.t2s[i]
+                '0
             ));
         endproperty
         // property ex_clear;
