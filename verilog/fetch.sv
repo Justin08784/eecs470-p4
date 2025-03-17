a5ac129c9c6ac571f92f970350004642051a5c57
jsstchur <jsstchur@umich.edu>
2025-03-17 14:50:31 -0400

    fetch works! for real!

--- verilog/fetch.sv
+++ verilog/fetch.sv
@@ -56,22 +56,22 @@ module stage_if (
 
     logic [$clog2(`N):0] if_valid_q;
 
-    fifo #(
-        .DEPTH(16),
-        .WIDTH($bits(INST)),
-        .NUM_RPORTS(`N),
-        .NUM_WPORTS(`N),
-        .MAX_SCNT(`N)
-    ) dut (
-        .clock      (clock),
-        .reset      (reset),
-        .wr_en_cnt  (free_scnt),
-        .wr_data    (Imem_data),
-        .rd_en_cnt  (fetch_in.d_rdy_cnt),
-        .rd_data    (fifo_insns),
-        .free_scnt  (free_scnt),
-        .used_scnt  (used_scnt)
-    );
+    // fifo #(
+    //     .DEPTH(16),
+    //     .WIDTH($bits(INST)),
+    //     .NUM_RPORTS(`N),
+    //     .NUM_WPORTS(`N),
+    //     .MAX_SCNT(`N)
+    // ) dut (
+    //     .clock      (clock),
+    //     .reset      (reset),
+    //     .wr_en_cnt  (free_scnt),
+    //     .wr_data    (Imem_data),
+    //     .rd_en_cnt  (fetch_in.d_rdy_cnt),
+    //     .rd_data    (fifo_insns),
+    //     .free_scnt  (free_scnt),
+    //     .used_scnt  (used_scnt)
+    // );
 
     // genvar i;
     // generate
@@ -89,7 +89,7 @@ module stage_if (
     //RE-EVALUATE
     always_comb begin
         //if (icache_valid) begin
-            if (if_valid_q == 2 && PC_reg % 8 != 0) begin
+            if (/*if_valid_q*/fetch_in.d_rdy_cnt == 2'b10 && PC_reg % 8 != 0) begin
                 fetch_out.f_en_cnt = 2'b01;
             end else begin
                 fetch_out.f_en_cnt = fetch_in.d_rdy_cnt;
@@ -108,7 +108,7 @@ module stage_if (
         end else if (take_branch) begin
             PC_reg <= branch_target; // update to a taken branch (does not depend on valid bit)
             PC_reg4 <= branch_target + 4;
-        end else if (|fetch_out.f_en_cnt) begin
+        end else begin
             PC_reg <= PC_reg + (fetch_out.f_en_cnt * 4);    // or transition to next PC if valid
             PC_reg4 <= PC_reg4 + (fetch_out.f_en_cnt * 4);
         end
@@ -116,8 +116,8 @@ module stage_if (
 
     // index into the word (32-bits) of memory that matches this instruction
     // FOR SUPERSCALAR: take ENTIRE BLOCK instead of pulling based on PC_reg % 8
-    assign fetch_out.f_dat[0].inst = (fetch_out.f_en_cnt != 0) ? fifo_insns[0] : `NOP;
-    assign fetch_out.f_dat[1].inst = (fetch_out.f_en_cnt == 2) ? fifo_insns[1] : `NOP;
+    assign fetch_out.f_dat[0].inst = (fetch_out.f_en_cnt != 2'b00) ? Imem_data[1].word_level[PC_reg[2]] : `NOP;
+    assign fetch_out.f_dat[1].inst = (fetch_out.f_en_cnt == 2'b10) ? Imem_data[0].word_level[PC_reg4[2]] : `NOP;
 
     assign fetch_out.f_dat[0].PC  = PC_reg;
     assign fetch_out.f_dat[0].NPC = PC_reg + 4; // pass PC+4 down pipeline w/instruction
@@ -125,7 +125,7 @@ module stage_if (
     assign fetch_out.f_dat[1].NPC = PC_reg4 + 4; // pass PC+4 down pipeline w/instruction
 
     assign fetch_out.f_dat[0].valid = |fetch_out.f_en_cnt;
-    assign fetch_out.f_dat[1].valid = fetch_out.f_en_cnt == 2;
+    assign fetch_out.f_dat[1].valid = fetch_out.f_en_cnt == 2'b10;
     //     end
     // endgenerate
 
