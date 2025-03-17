803d59655e7808daa9460cdf02a15559d13a09c6
Justin Shin <shine777justin@gmail.com>
2025-03-17 04:36:41 -0400

    stage_id tests: write_read_correctly; but some #1step wonkiness

--- test/stage_id_p4_sva.svh
+++ test/stage_id_p4_sva.svh
@@ -15,11 +15,14 @@ module stage_id_p4_sva (
     localparam DEPTH = 2*`N;
 
     int     rd_count; // number of reads complete
+    int     wr_count; // number of writs complete
     logic   [$clog2(DEPTH):0] used;    // how full the buffer should be
     logic   [$clog2(DEPTH):0] free;    // how full the buffer should be
     assign  free = DEPTH - used;
 
 
+    decode2dispatch f_out_delayed;
+    decode2dispatch d_out_delayed;
     initial begin
         // wait until 1st reset: ensures no Xs are floating around
         // (if there are Xs we get errors like indexing with Xs into assoc. arrays)
@@ -37,6 +40,15 @@ module stage_id_p4_sva (
         //     rd_data_sva[i] = entries.pop_front();
         // end
         // #0
+
+        /*
+        This #1step ensures that when the property checks d_out in the same cycle that new data is written, 
+        it will see the forwarded value (instead of seeing the old/stale value too early).
+
+        But why the fuck don't we need this for fifo_sva.svh????
+        */
+        #1step f_out_delayed = f_out; 
+        #1step d_out_delayed = d_out; 
         @(posedge clock);
         @(negedge clock);
     end
@@ -45,15 +57,31 @@ module stage_id_p4_sva (
     always_ff @(posedge clock) begin
         if (reset) begin
             rd_count    <= 0;
+            wr_count    <= 0;
             used        <= 0;
         end else begin
             rd_count    <= rd_count + d_in.dispatch_en_cnt;
+            wr_count    <= wr_count + f_in.f_en_cnt;
             used        <= used + f_in.f_en_cnt - d_in.dispatch_en_cnt;
         end
     end
 
     task exit_on_error;
         begin
+            $display("f_cnt: %d d_cnt: %d", f_in.f_en_cnt, d_in.dispatch_en_cnt);
+            $display("  %3d | d_in: PC[%x, %x] inst[%x, %x] wr_en_cnt: %d  rd_en_cnt: %d  |  d_out: PC[%x, %x] inst[%x,%x]",
+                $time,
+                f_in.f_en_cnt > 0 ? f_in.f_dat[0].PC : 0,
+                f_in.f_en_cnt > 1 ? f_in.f_dat[1].PC : 0,
+                f_in.f_en_cnt > 0 ? f_in.f_dat[0].inst : 0,
+                f_in.f_en_cnt > 1 ? f_in.f_dat[1].inst : 0,
+                f_in.f_en_cnt,
+                d_in.dispatch_en_cnt,
+                d_out.d_dat[0].PC, 
+                d_out.d_dat[1].PC,
+                d_out.d_dat[0].inst, 
+                d_out.d_dat[1].inst
+            );
             $display("\n\033[31m@@@ Failed at time %4d\033[0m\n", $time);
             // $display("used %d free %d us %d fs %d reset: %b", used, free, used_scnt, free_scnt, reset);
             $finish;
@@ -68,7 +96,7 @@ module stage_id_p4_sva (
 
         property wr_en_correct;
             disable iff (reset)
-            f_in.f_en_cnt <= used + d_in.dispatch_en_cnt;
+            f_in.f_en_cnt <= free + d_in.dispatch_en_cnt;
         endproperty
 
         // property used_scnt_correct;
@@ -86,13 +114,36 @@ module stage_id_p4_sva (
         //     rd_data == rd_data_sva;
         // endproperty
 
-        // property write_read_correctly(i);
-        //     logic [WIDTH-1:0] data_in;
-        //     int               idx;
-        //     (wr_en_cnt > i, data_in=wr_data[i], idx=(rd_count + used + i)) // value is written
-        //     ##[1:$] (rd_en_cnt > 0 && rd_count <= idx && idx < rd_count + rd_en_cnt) // wait for previous entries to be read
-        //     |-> rd_data[idx - rd_count] === data_in;              // ensure correct value out
-        // endproperty
+        property write_read_correctly(i);
+            INST inst_in;
+            ADDR PC_in;
+            int  idx;
+            (f_in.f_en_cnt > i, inst_in=f_in.f_dat[i].inst, PC_in=f_in.f_dat[i].PC, idx=(rd_count + used + i),
+                $display("INP f_en_cnt: %d, i: %0d, f_in[%0d].PC: %x, rd_count: %0d, used: %0d, idx: %0d",
+                    f_in.f_en_cnt,
+                    i,
+                    i,
+                    f_in.f_dat[i].PC,
+                    rd_count,
+                    used,
+                    idx
+                )
+            ) // value is written
+            ##[0:$] (d_in.dispatch_en_cnt > 0 && rd_count <= idx && idx < rd_count + d_in.dispatch_en_cnt,
+                $display("CHK d_en_cnt: %d, PC: [%x, %x], rd_count: %0d, used: %0d",
+                    d_in.dispatch_en_cnt,
+                    d_out.d_dat[0].PC,
+                    d_out.d_dat[1].PC,
+                    rd_count,
+                    used
+                )
+            ) // wait for previous entries to be read
+            |-> (
+                d_out_delayed.d_dat[idx - rd_count].PC === PC_in
+                && d_out_delayed.d_dat[idx - rd_count].inst === inst_in
+                && d_out_delayed.d_dat[idx - rd_count].id === rd_count + idx - rd_count // ensures id increments
+            );
+        endproperty
 
     endclocking
 
@@ -107,11 +158,12 @@ module stage_id_p4_sva (
     //     else exit_on_error;
     // RdData: assert property(cb.rd_data_correct)
     //     else exit_on_error;
-    // generate
-    //     for (genvar wr_port = 0; wr_port < NUM_WPORTS; ++wr_port) begin : gen_wr_props
-    //         assert property(cb.write_read_correctly(wr_port));
-    //     end
-    // endgenerate
+    generate
+        for (genvar wr_port = 0; wr_port < `N; ++wr_port) begin : gen_wr_props
+            assert property(cb.write_read_correctly(wr_port))
+                else exit_on_error;
+        end
+    endgenerate
 
 endmodule
 
--- test/stage_id_p4_test.sv
+++ test/stage_id_p4_test.sv
@@ -24,23 +24,28 @@ module stage_id_p4_test();
     // Generate nonzero random numbers for our write data on each cycle
     // (we shall treat a 0 as non-enabled; this allows us to print 0s in the $monitor
     // to indicate non-enabled)
+    IF_ID_PACKET [`N-1:0] tmp;
     always @(negedge clock) begin
-        // std::randomize(f_in);
+        std::randomize(tmp);
+        f_in.f_dat = tmp;
     end
 
     logic DEBUG = 1;
     always @(posedge clock) begin
         if (DEBUG) begin
-            // $display("  %3d | d_in: [%d, %d]   wr_en_cnt: %d  rd_en_cnt: %d  |  d_out: [%d, %d]   used_scnt: %2d  free_scnt: %2d",
-            //     $time,
-            //     wr_en_cnt > 0 ? wr_data[0] : 0,
-            //     wr_en_cnt > 1 ? wr_data[1] : 0,
-            //     wr_en_cnt,
-            //     rd_en_cnt,
-            //     rd_data[0], 
-            //     rd_data[1], 
-            //     used_scnt, 
-            //     free_scnt);
+            $display("  %3d | d_in: PC[%x, %x] inst[%x, %x] wr_en_cnt: %d  rd_en_cnt: %d  |  d_out: PC[%x, %x] inst[%x,%x]",
+                $time,
+                f_in.f_en_cnt > 0 ? f_in.f_dat[0].PC : 0,
+                f_in.f_en_cnt > 1 ? f_in.f_dat[1].PC : 0,
+                f_in.f_en_cnt > 0 ? f_in.f_dat[0].inst : 0,
+                f_in.f_en_cnt > 1 ? f_in.f_dat[1].inst : 0,
+                f_in.f_en_cnt,
+                d_in.dispatch_en_cnt,
+                d_out.d_dat[0].PC, 
+                d_out.d_dat[1].PC,
+                d_out.d_dat[0].inst, 
+                d_out.d_dat[1].inst
+            );
         end
     end
     
@@ -85,6 +90,52 @@ module stage_id_p4_test();
         @(negedge clock);
         reset = 0;
         @(negedge clock);
+
+
+        // ---------- Test 15 ---------- //
+        $display("\nTest 15: Forwarding");
+
+        $display("1 read: from state");
+        f_in.f_en_cnt = 1;
+        @(negedge clock);
+        f_in.f_en_cnt = 0;
+
+        d_in.dispatch_en_cnt = 1;
+        @(negedge clock);
+        d_in.dispatch_en_cnt = 0;
+
+        $display("1 read: fwded");
+        f_in.f_en_cnt = 1;
+        d_in.dispatch_en_cnt = 1;
+        @(negedge clock);
+        f_in.f_en_cnt = 0;
+        d_in.dispatch_en_cnt = 0;
+
+        $display("2 reads: both from state");
+        f_in.f_en_cnt = 2;
+        @(negedge clock);
+        f_in.f_en_cnt = 0;
+
+        d_in.dispatch_en_cnt = 2;
+        @(negedge clock);
+        d_in.dispatch_en_cnt = 0;
+
+        $display("2 reads: 1 from state, 1 fwded");
+        f_in.f_en_cnt = 1;
+        @(negedge clock);
+
+        d_in.dispatch_en_cnt = 2;
+        @(negedge clock);
+        f_in.f_en_cnt = 0;
+        d_in.dispatch_en_cnt = 0;
+
+        $display("2 reads: both fwded");
+        f_in.f_en_cnt = 2;
+        d_in.dispatch_en_cnt = 2;
+        @(negedge clock);
+        f_in.f_en_cnt = 0;
+        d_in.dispatch_en_cnt = 0;
+
 
 
 
