8f1790d3d0669c709b38194c9fde78cadf01c26d
Ethan R Sanders <ethansan@caen-vnc-mi11.engin.umich.edu>
2025-03-20 15:38:07 -0400

    addi's now work properly

--- verilog/execute.sv
+++ verilog/execute.sv
@@ -79,6 +79,10 @@ module stage_ex_p4 (
         ID_RESULT   [`NUM_FU_ALU-1:0]   dat;
     } alu_ins;
     struct packed {
+        DATA [`NUM_FU_ALU-1:0] opa_mux_out, opb_mux_out;
+        logic [`NUM_FU_ALU-1:0] [2:0] branch_func;
+    } alu_operands;
+    struct packed {
         logic       [`NUM_FU_MULT-1:0]  bsy;
         ID_RESULT   [`NUM_FU_MULT-1:0]  dat;
     } mul_ins;
@@ -102,17 +106,65 @@ module stage_ex_p4 (
     DST     [`NUM_FU_MULT-1:0] mul_dst_n;
     logic   [`NUM_FU_MULT-1:0] mul_vld_n;
 
+
+    always_comb begin
+        foreach(rs_in.fu_dat_alu[i]) begin
+            if(!rs_in.fu_vld_alu[i]) 
+                continue;
+
+            // ex_2_prf.prf_en[i] = rs_in.fu_vld_alu[i];
+            // ex_2_prf.s_t1s[i] = rs_in.fu_dat_alu[i].t1;
+            // ex_2_prf.s_t2s[i] = rs_in.fu_dat_alu[i].t2;
+
+
+            if (alu_ins.dat[i].cond_branch) begin
+                alu_operands.opa_mux_out[i] = prf_in.s_v1s[i];
+                alu_operands.opb_mux_out[i] = prf_in.s_v2s[i];
+                // alu_func = 4'ha; //SENTINEL VALUE
+                alu_operands.branch_func[i] = rs_in.fu_dat_alu[i].inst.b.funct3;
+                // branch = 1;
+            end else begin
+                // ALU opA mux
+                case (rs_in.fu_dat_alu[i].opa_select)
+                    // OPA_IS_RS1:  alu_operands.opa_mux_out[i][i] = rs_in.fu_dat_alu[i].rs1_value;
+                    OPA_IS_RS1:  alu_operands.opa_mux_out[i] = prf_in.s_v1s[i];
+                    OPA_IS_NPC:  alu_operands.opa_mux_out[i] = rs_in.fu_dat_alu[i].NPC;
+                    OPA_IS_PC:   alu_operands.opa_mux_out[i] = rs_in.fu_dat_alu[i].PC;
+                    OPA_IS_ZERO: alu_operands.opa_mux_out[i] = 0;
+                    default:     alu_operands.opa_mux_out[i]= 32'hdeadface; // dead face
+                endcase
+
+                // ALU opB mux
+                case (rs_in.fu_dat_alu[i].opb_select)
+                    // OPB_IS_RS2:   alu_operands.opb_mux_out[i][i] = rs_in.fu_dat_alu[i].rs2_value;
+                    OPB_IS_RS2:   alu_operands.opb_mux_out[i] =  prf_in.s_v2s[i];
+                    OPB_IS_I_IMM: alu_operands.opb_mux_out[i] = `RV32_signext_Iimm(rs_in.fu_dat_alu[i].inst);
+                    OPB_IS_S_IMM: alu_operands.opb_mux_out[i] = `RV32_signext_Simm(rs_in.fu_dat_alu[i].inst);
+                    OPB_IS_B_IMM: alu_operands.opb_mux_out[i] = `RV32_signext_Bimm(rs_in.fu_dat_alu[i].inst);
+                    OPB_IS_U_IMM: alu_operands.opb_mux_out[i] = `RV32_signext_Uimm(rs_in.fu_dat_alu[i].inst);
+                    OPB_IS_J_IMM: alu_operands.opb_mux_out[i] = `RV32_signext_Jimm(rs_in.fu_dat_alu[i].inst);
+                    default:      alu_operands.opb_mux_out[i] = 32'hfacefeed; // face feed
+                endcase
+
+                // alu_func = rs_in.fu_dat_alu[i].alu_func;
+                alu_operands.branch_func[i] = 3'b011; //SENTINEL VALUE
+                // branch = 0;
+            end
+        end
+    end
+
+
     generate
         for (genvar i = 0; i < `NUM_FU_ALU; ++i) begin : gen_alus
             // // Instantiate the ALU
             // TODO: These ALU inputs were kinda hardcoded. Need mux stuff to select which type.
             alu alu_0 ( 
                 // Inputs
-                .opa(prf_in.s_v1s[i]),
-                .opb(prf_in.s_v2s[i]),
+                .opa(alu_operands.opa_mux_out[i]),
+                .opb(alu_operands.opb_mux_out[i]),
                 .alu_func(alu_ins.dat[i].alu_func),
                 .branch(alu_ins.dat[i].cond_branch), // is this a cond_branch
-                .branch_func(3'b011), // Which branch condition to check
+                .branch_func(alu_operands.branch_func[i]), // Which branch condition to check
 
                 .take(), // True/False condition result (will return FALSE if branch is low)
                 .result(alu_res_n[i]) // will return 32'hfacebeec if branch is high (Sentinel, hopefully none of our alu computations result in that value)
