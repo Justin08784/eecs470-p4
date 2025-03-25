023d49e2a7c7a417059eea16ee2df4227d6df22b
Justin Shin <shine777justin@gmail.com>
2025-03-25 04:35:00 -0400

    test3: many indep mul

---
+++ programs/test3.s
@@ -0,0 +1,14 @@
+    # addi x2, x0, 2
+    # addi x3, x0, 3
+    # addi x4, x0, 4
+    # addi x5, x0, 5
+    # addi x6, x0, 6
+    # addi x7, x0, 7
+
+    mul x2, x0, x0
+    mul x3, x0, x0
+    mul x4, x0, x0
+    mul x5, x0, x0
+    mul x6, x0, x0
+    mul x7, x0, x0
+    wfi
