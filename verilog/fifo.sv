a72b4eda07a6fec1692ae1fb34625a35812942ca
Justin Shin <shine777justin@gmail.com>
2025-03-26 17:19:01 -0400

    fifo: clearer ENABLE_FREE_LIST_MODE configuration

--- verilog/fifo.sv
+++ verilog/fifo.sv
@@ -122,15 +122,22 @@ module fifo #(
     end
 
     always_ff @(posedge clock) begin
-        if (reset || (flush && !ENABLE_FREE_LIST_MODE)) begin
+        if (reset) begin
             used    <= RESET_STATE.used;
             head    <= RESET_STATE.head;
             tail    <= RESET_STATE.tail;
             state   <= RESET_STATE.state;
-        end else if (flush && ENABLE_FREE_LIST_MODE) begin
-            // advance tail to head and mark entire FIFO as used (i.e. full with entries)
-            used    <= DEPTH;
-            tail    <= head;
+        end else if (flush) begin
+            if (ENABLE_FREE_LIST_MODE) begin
+                // advance tail to head and mark entire FIFO as used (i.e. full with entries)
+                used    <= DEPTH;
+                tail    <= head;
+            end else begin
+                used    <= RESET_STATE.used;
+                head    <= RESET_STATE.head;
+                tail    <= RESET_STATE.tail;
+                state   <= RESET_STATE.state;
+            end
         end else begin
             if (wr_en_cnt > (ENABLE_INTR_FWD ? free + rd_en_cnt : free))
                 $error("FIFO overflow! instance: %d", INSTANCE_ID);
