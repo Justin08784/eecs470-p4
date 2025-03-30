d96ffecfb6689505e7448594a765d8c4a5cc7bbf
Justin Shin <shine777justin@gmail.com>
2025-03-29 20:25:52 -0400

    branchy annotated

--- programs/branchy.s
+++ programs/branchy.s
@@ -1,8 +1,8 @@
-    addi x2, x0, 5      # x2 = 5 (loop counter)
+    addi x2, x0, 5      # 0: x2 = 5 (loop counter)
 loop:
-    addi x3, x2, 3      # x3 = x2 + 3 (just some operation)
+    addi x3, x2, 3      # 4: x3 = x2 + 3 (just some operation)
     # mul x4, x2, x3      # x4 = x2 * x3
-    add x5, x2, x3      # x5 = x2 + x3
-    addi x2, x2, -1     # decrement counter
-    bnez x2, loop       # if x2 != 0, jump back to loop
-    wfi                 # wait for interrupt
+    add x5, x2, x3      # 8: x5 = x2 + x3
+    addi x2, x2, -1     # c: decrement counter
+    bnez x2, loop       # 10: if x2 != 0, jump back to loop
+    wfi                 # 14: wait for interrupt
