d8a7e13b6f8f6de7033af7e7de4f22faef681ba0
Justin Shin <shine777justin@gmail.com>
2025-03-31 15:30:13 -0400

    bradley's advice to optimize rs_rdy_scnt

--- verilog/rs.sv
+++ verilog/rs.sv
@@ -271,6 +271,7 @@ module rs #(parameter
     assign free_entries = 
         ~busy_vec
         | issd_vec; // an issued insn will go to EX and free its entry
+    // Bradley: TODO: make this a sat count that stops at 2N (make it a dep for loop); 2N so you can fit reservations as well
     assign rs_cnt = $countones(free_entries);
     always_ff @(posedge clock) begin
         if (rsrv_cnt > rs_cnt && !reset)
