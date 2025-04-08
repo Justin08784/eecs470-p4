b410fb403e645f22e6eb47f953dec34a60d9e614
Ethan R Sanders <ethansan@caen-vnc-mi04.engin.umich.edu>
2025-04-08 19:40:08 -0400

    Removed some parameters from ROB_ENTRY, SQ_ENTRY, and LQ_ENTRY that aren't needed. Updated ld-st ooo flag logic in LQ to directly interface with retire rather than ROB

--- verilog/cpu.sv
+++ verilog/cpu.sv
@@ -338,6 +338,7 @@ module cpu (
     //////////////////////////////////////////////////
 
     execute2lq exec_2_lq;
+    execeuteST2lq execST_2_lq;
 
     lq lq_0(
         `ifdef DEBUG
@@ -350,6 +351,7 @@ module cpu (
         .dis_2_lq(dis_2_lq),
         .retire_2_lq(retire_2_lq),
         .exec_2_lq(exec_2_lq),
+        .execST_in(execST_2_lq),
 
         .lq_2_dis(lq_2_dis),
         .lq_2_rob(lq_2_rob),
@@ -374,6 +376,7 @@ module cpu (
         .sq_out (exec_2_sq),
 
         .lq_out (exec_2_lq),
+        .st_lq_out (execST_2_lq),
 
         .prf_in (prf_2_ex),
         .prf_out(ex_2_prf),
