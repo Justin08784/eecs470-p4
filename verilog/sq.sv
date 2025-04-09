c742f4482311c8594455d6d5c77f4a0b5f2bd6ff
Justin Shin <shine777justin@gmail.com>
2025-04-09 18:58:55 -0400

    minor bug fix: nonblocking assigns in comb blocks

--- verilog/sq.sv
+++ verilog/sq.sv
@@ -84,7 +84,7 @@ module sq #(parameter
             next_ids[i] = (tail_dbl + i) % LSQ_SZ_DBL;
 
         // handle dispatch (outs)
-        sq_2_dis <= '{
+        sq_2_dis = '{
             sq_rdy_scnt         : free_scnt,
             last_used_sq_idx    : last_used_sq_idx,
             next_ids            : next_ids,
