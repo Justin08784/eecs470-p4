569f68a78c17e94c08b01a9ffcbd9f2efcf7a08b
Justin Shin <shine777justin@gmail.com>
2025-04-13 23:06:58 -0400

    more test changes

--- test/dcache_test.sv
+++ test/dcache_test.sv
@@ -15,6 +15,17 @@ function automatic string dbg_mem_cmd(input MEM_COMMAND cmd);
     return rv;
 endfunction
 
+function automatic string dbg_mem_size(input MEM_SIZE size);
+    string rv;
+    case (size)
+        BYTE:   rv = "BYTE";
+        HALF:   rv = "HALF";
+        WORD:   rv = "WORD";
+        DOUBLE: rv = "DBLE";
+    endcase
+    return rv;
+endfunction
+
 module dcache_test;
     // DBG
     DBG_cache dbg;
@@ -120,7 +131,7 @@ module dcache_test;
         $display("  %3d | >> memDP", $time);
         for (int s = 0; s < NUM_SETS; ++s) begin
             $display("Set=%1d. age=%b", s, dbg.hdr.age[s]);
-            for (int w = 0; w < ASSOC; ++w) begin
+            for (int w = 0; w == 0; ++w) begin
                 $display("  vld=%b, dirty=%b, tag=%x: data=%x",
                     dbg.hdr.vld[s][w],
                     dbg.hdr.dirty[s][w],
@@ -133,61 +144,75 @@ module dcache_test;
     endtask
 
     // Clock generation
-    localparam DCACHE_CLOCK = 20;
+    logic enable_prints = 0;
+    localparam DCACHE_CLOCK = 50;
     always begin
         #(DCACHE_CLOCK/2);
         clock = ~clock;
     end
     always @(negedge clock) begin
-        // #0;
-        $display("lod in: {vld: %b, addr: %4x, size: %1d}",
-            ld_vld,
-            ld_addr,
-            ld_size
-        );
+        if (enable_prints) begin
+            // #0;
+            $display("lod in: {vld: %b, addr: %4x, size: %s}",
+                ld_vld,
+                ld_addr,
+                dbg_mem_size(ld_size)
+            );
 
-        $display("lod ot: {status: %b, dat: %x}",
-            ld_status,
-            ld_dat
-        );
+            $display("lod ot: {status: %b, dat: %x}",
+                ld_status,
+                ld_dat
+            );
 
-        $display("str in: {vld: %b, addr: %4x, size: %1d, dat: %x}",
-            st_vld,
-            st_addr,
-            st_size,
-            st_dat
-        );
+            $display("str in: {vld: %b, addr: %4x, size: %s, dat: %x}",
+                st_vld,
+                st_addr,
+                dbg_mem_size(st_size),
+                st_dat
+            );
 
-        $display("str ot: {status: %b}",
-            st_status
-        );
+            $display("str ot: {status: %b}",
+                st_status
+            );
 
-        $display("mem in: {txn_tag: %2d, dat: %x, dat_tag: %2d}",
-            mem_in_transaction_tag,
-            mem_in_data,
-            mem_in_data_tag
-        );
-        $display("vld addres: %b", memory.valid_address);
+            $display("mem in: {txn_tag: %2d, dat: %x, dat_tag: %2d}",
+                mem_in_transaction_tag,
+                mem_in_data,
+                mem_in_data_tag
+            );
+            $display("vld addres: %b", memory.valid_address);
 
-        $display("mem ot: {cmd: %s, addr: %4x, dat: %x}",
-            dbg_mem_cmd(mem_out_command),
-            mem_out_addr,
-            mem_out_data
-        );
+            $display("mem ot: {cmd: %s, addr: %4x, dat: %x}",
+                dbg_mem_cmd(mem_out_command),
+                mem_out_addr,
+                mem_out_data
+            );
 
-        // $display("st_vld: %b", dut.st_vld);
-        // $display("st_addr: %x", dut.st_addr);
-        // $display("st_size: %1d", dut.st_size);
-        // $display("st_status: %b", dut.st_status);
-        // $display("st_dat: %x", dut.st_dat);
-        $display("req: %b", dut.req);
-        $display("gnt: %b", dut.gnt);
-        // print_dbg();
-        $display("");
+            // $display("st_vld: %b", dut.st_vld);
+            // $display("st_addr: %x", dut.st_addr);
+            // $display("st_size: %1d", dut.st_size);
+            // $display("st_status: %b", dut.st_status);
+            // $display("st_dat: %x", dut.st_dat);
+            $display("req: %b", dut.req);
+            $display("gnt: %b", dut.gnt);
+            print_dbg();
+            $display("");
+        end
     end
 
     initial begin
         logic [3:0][15:0] half_template;
+
+        $display("Starting dcache testbench...");
+        clock = 0;
+        reset = 0;
+        clr_inputs();
+
+        $display("\n  %16t : Asserting Reset", $realtime);
+        reset = 1;
+        for (int i = 0; i < 6; ++i)
+            @(negedge clock);
+        $display("  %16t : Loading Unified Memory", $realtime);
         for (logic [31:0] i = 0; i < `MEM_64BIT_LINES; ++i) begin
             if (i >= 256) begin
                 memory.unified_memory[i] = '0;
@@ -199,22 +224,15 @@ module dcache_test;
             memory.unified_memory[i] = half_template;
             // $display("mem[%4x]: %x", 8*i, memory.unified_memory[i]);
         end
-
-        $display("Starting dcache testbench...");
-        clock = 0;
-        reset = 0;
-        clr_inputs();
-
-        reset = 1;
-        for (int i = 0; i < 6; ++i)
-            @(negedge clock);
         #1;
+        $display("  %16t : Deasserting Reset", $realtime);
         reset = 0;
+        enable_prints = 1;
 
-        wr(4, 'hbeeffeed, WORD);
+        wr('h80, 'hbeeffeed, WORD);
         @(negedge clock);
         clr_inputs();
-        for (int i = 0; i < 20; ++i)
+        for (int i = 0; i < 10; ++i)
             @(negedge clock);
 
         $display("Finished dcache testbench.");
