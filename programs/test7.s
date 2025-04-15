4aeefe43d584f9007cb05275b4e1c24f0003b04a
Ethan R Sanders <ethansan@caen-vnc-mi04.engin.umich.edu>
2025-04-15 17:02:27 -0400

    Updated test7 that causes an overwrite on the wrong address in dcache with a store

--- programs/test7.s
+++ programs/test7.s
@@ -1,25 +1,29 @@
     li x2, 1
-    sw x2, 120(x0)
+    sw x2, 1024(x0)
     li x2, 2
-    sw x2, 128(x0)
+    sw x2, 1032(x0)
     li x2, 3
-    sw x2, 132(x0)
+    sw x2, 1036(x0)
     li x2, 4
-    sw x2, 136(x0)
+    sw x2, 1040(x0)
     li x2, 5
-    sw x2, 140(x0)
+    sw x2, 1044(x0)
     li x2, 6
-    sw x2, 144(x0)
+    sw x2, 1048(x0)
     li x2, 7
-    sw x2, 148(x0)
+    sw x2, 1052(x0)
     li x2, 8
-    sw x2, 152(x0)
+    sw x2, 1056(x0)
     li x2, 9
-    sw x2, 156(x0)
+    sw x2, 1060(x0)
     li x2, 10
-    sw x2, 160(x0)
+    sw x2, 1064(x0)
     li x2, 11
-    sw x2, 168(x0)
+    sw x2, 1072(x0)
+    li x2, 12
+    sw x2, 1074(x0)
+    li x2, 13
+    sw x2, 1078(x0)
     nop
     nop
     nop
