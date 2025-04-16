a6e11c53ce37c7b8fa141b0b9c0eed676bd82bbf
Ethan R Sanders <ethansan@caen-vnc-mi17.engin.umich.edu>
2025-04-16 08:08:31 -0400

    Raising size of LSQ to 12 to match what it was when the ret buf was separate

--- verilog/sys_defs.svh
+++ verilog/sys_defs.svh
@@ -37,8 +37,8 @@
 
 // worry about these later
 `define BRANCH_PRED_SZ xx
-`define LSQ_SZ 8
-`define LSQ_SZ_DBL 16
+`define LSQ_SZ 12
+`define LSQ_SZ_DBL 24
 `define SQ_RET_BUF_SZ 4
 
 // functional units (you should decide if you want more or fewer types of FUs)
