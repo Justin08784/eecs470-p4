cedeab3e72f474be745af48cf430be2d8affd17b
Justin Shin <shine777justin@gmail.com>
2025-04-16 01:29:18 -0400

    all_test.sh: simple script to test various non-meory tests

---
+++ all_test.sh
@@ -0,0 +1,7 @@
+make test1.out test2.out test3.out mult_no_lsq.out btest1.out btest2.out
+./ck.sh test1 wb
+./ck.sh test2 wb
+./ck.sh test3 wb
+./ck.sh mult_no_lsq wb
+./ck.sh btest1 wb
+./ck.sh btest2 wb
