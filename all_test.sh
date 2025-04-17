fe3576876d97970a02625744dad57f90a4333bff
Justin Shin <shine777justin@gmail.com>
2025-04-17 15:41:59 -0400

    updated all_test

--- all_test.sh
+++ all_test.sh
@@ -1,7 +1,21 @@
-make test1.out test2.out test3.out mult_no_lsq.out btest1.out btest2.out
+make test1.out \
+    test2.out \
+    test3.out \
+    mult_no_lsq.out \
+    btest1.out \
+    btest2.out \
+    evens.out \
+    sampler.out \
+    copy.out \
+    mult_orig.out
+
 ./ck.sh test1 wb
 ./ck.sh test2 wb
 ./ck.sh test3 wb
 ./ck.sh mult_no_lsq wb
 ./ck.sh btest1 wb
 ./ck.sh btest2 wb
+./ck.sh evens wb
+./ck.sh sampler wb
+./ck.sh copy wb
+./ck.sh mult_orig wb
