1c3d9a580479bc2232e4eeab754d95670f3ca5af
Justin Shin <shine777justin@gmail.com>
2025-04-21 02:14:18 -0400

    all_test now writes to diff files

--- all_test.sh
+++ all_test.sh
@@ -43,6 +43,7 @@ make ${TESTS[@]/%/.out}
 for t in "${TESTS[@]}"; do
     for ext in "${EXTS[@]}"; do
         echo "Diffing $t.$ext..."
-        diff "correct_out/${t}.${ext}" "output/${t}.${ext}"
+        diff "correct_out/${t}.${ext}" "output/${t}.${ext}" > "diffs/${t}.${ext}.diff"
+        # diff "correct_out/${t}.${ext}" "output/${t}.${ext}"
     done
 done
