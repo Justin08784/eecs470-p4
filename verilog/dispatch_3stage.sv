5b7d2cc4ea6d3ced8f6eff7a8c9ad103d36b7cfc
Justin Shin <shine777justin@gmail.com>
2025-05-18 03:20:47 -0400

    reorg: alloc-rename joined

--- verilog/dispatch_3stage.sv
+++ verilog/dispatch_3stage.sv
@@ -42,6 +42,10 @@ module dispatch #(parameter
     logic [`PHYS_REG_SZ_R10K-1:0] cpl_lst;
 
     /* >> ==== 1. Alloc Stage ==== >> */
+    logic [$clog2(N):0] alloc_en_cnt;
+    logic [$clog2(N):0] alloc_rdy_scnt;
+    logic [$clog2(N):0] alloc_vld_scnt;
+
     // Gate by availability
     logic [`N-1:0] has_dst;
     logic [`N:0][$clog2(`N):0] free_prefix_cnt;
@@ -58,76 +62,28 @@ module dispatch #(parameter
     always_comb begin
         foreach (has_dst[n])
             has_dst[n] = d_in.d_dat[n].has_dst;
+
+        // TODO: Is syntheizer smart enough to transform this MIN compute into a tree?
+        alloc_en_cnt = d_in.d_vld_scnt;
+        alloc_en_cnt = `MIN(rob_in.rob_rdy_scnt, alloc_en_cnt);
+        alloc_en_cnt = `MIN(free_lim_cnt, alloc_en_cnt);
+        alloc_en_cnt = `MIN(alloc_rdy_scnt, alloc_en_cnt);
+
+        d_out.dispatch_en_cnt  = alloc_en_cnt;
+        rob_out.alloc_en_cnt   = alloc_en_cnt;
+        free_out.free_d_en_cnt = free_prefix_cnt[alloc_en_cnt];
     end
 
-    logic [$clog2(N):0] rename_vld_scnt;
-    logic [$clog2(N):0] rename_rdy_scnt;
-    logic [$clog2(N):0] rename_en_cnt;
-    logic [`N-1:0]      rename_en;
-
-    /* >> ==== 2. Rename Stage ==== >> */
-
-    logic [`N-1:0] rnme_is_brch;
-    logic [`N:0][$clog2(`N):0] rnme_snap_prefix_cnt;
-    logic [$clog2(`N):0] rnme_snap_lim_cnt;
-    compactor #(
-        .WIDTH(`N)
-    ) comp_rnme_snap (
-        .req        (rnme_is_brch),
-        .rdy        (bman_in.snap_rdy_scnt),
-        .gnt_cnt    (rnme_snap_lim_cnt),
-        .prefix_cnt (rnme_snap_prefix_cnt)
-    );
-
+    ALLOC_RENAME_PKT [`N-1:0] tmp_decode2alloc;
     always_comb begin
-        foreach (rnme_is_brch[n])
-            rnme_is_brch[n] = d_in.d_dat[n].fu_idx == FU_BRU;
+        int rd_idx;
 
-        rename_en_cnt = d_in.d_vld_scnt;
-        rename_en_cnt = `MIN(rob_in.rob_rdy_scnt, rename_en_cnt);
-        rename_en_cnt = `MIN(free_lim_cnt, rename_en_cnt);
-
-        // rename_en_cnt = `MIN(alloc_rdy_scnt, rename_en_cnt);
-        // rename_en_cnt = alloc_vld_scnt;
-
-        rename_en_cnt = `MIN(rename_rdy_scnt, rename_en_cnt);
-        rename_en_cnt = `MIN(rnme_snap_lim_cnt, rename_en_cnt);
-
-        d_out.dispatch_en_cnt  = rename_en_cnt;
-        rob_out.alloc_en_cnt   = rename_en_cnt;
-        free_out.free_d_en_cnt = free_prefix_cnt[rename_en_cnt];
-        bman_out.snap_en_cnt = rnme_snap_prefix_cnt[rename_en_cnt];
-    end
-
-    // handle map table output 
-    always_comb begin
-        map_out = '0;
-        map_out.en_cnt  = rename_en_cnt;
-
-        for (int i = 0; i < rename_en_cnt; i++) begin
-            //handling dest register
-            map_out.ts[i]       = has_dst[i] ? free_in.d_ts[free_prefix_cnt[i]] : '0;
-            map_out.dsts[i]     = d_in.d_dat[i].has_dst
-                ? d_in.d_dat[i].inst.r.rd
-                : `ZERO_REG;
-            //handling src tags
-            map_out.src1s[i]    = d_in.d_dat[i].inst.r.rs1;
-            map_out.src2s[i]    = d_in.d_dat[i].inst.r.rs2;
-        end
-    end
-
-    RENAME_COMMIT_PKT [`N-1:0] tmp_alloc2rename;
-    BMASK             [`N-1:0] tmp_alloc2rename_bmask;
-    always_comb begin
-        logic [`N-1:0] rd_src1s;
-        logic [`N-1:0] rd_src2s;
-
-        tmp_alloc2rename = '0;
+        tmp_decode2alloc = '0;
         for (int i = 0; i < `N; ++i) begin
-            // logic [$bits(RENAME_COMMIT_PKT)-$bits(ALLOC_RENAME_PKT)-1:0] diff;
+            // logic [$bits(ALLOC_RENAME_PKT)-$bits(ID_RESULT)-1:0] diff;
             // diff = '0;
-            // tmp_alloc2rename[i] = RENAME_COMMIT_PKT'({d_in.d_dat[i], diff});
-            tmp_alloc2rename[i] = '{
+            // tmp_decode2alloc[i] = ALLOC_RENAME_PKT'({d_in.d_dat[i], diff});
+            tmp_decode2alloc[i] = '{
                 // from ID_RESULT
 `ifdef DEBUG
                 id          : d_in.d_dat[i].id,
@@ -148,7 +104,113 @@ module dispatch #(parameter
                 btq_idx     : d_in.d_dat[i].btq_idx,
 
                 // alloc
-                t           : '0,
+                t           : has_dst[i] ? free_in.d_ts[free_prefix_cnt[i]] : '0,
+                fl_head_snap: free_in.fl_heads_n[free_prefix_cnt[i]]
+            };
+
+        end
+    end
+
+    ALLOC_RENAME_PKT [`N-1:0]  rename_in;
+    logic [$clog2(N):0] rename_vld_scnt;
+    logic [$clog2(N):0] rename_rdy_scnt;
+    logic [$clog2(N):0] rename_en_cnt;
+    logic [`N-1:0]      rename_en;
+    fifo #(
+        .INSTANCE_ID(39),
+        .DEPTH(2*`N),
+        .WIDTH($bits(ALLOC_RENAME_PKT)),
+        .NUM_RPORTS(`N),
+        .NUM_WPORTS(`N),
+        .FLUSH_MODE(FIFO_FLUSH_RESET),
+        .ENABLE_INTR_FWD(`FALSE)
+    ) alloc_buf (
+        .clock      (clock),
+        .reset      (reset),
+        .flush      (flush),
+        .wr_en_cnt  (alloc_en_cnt),
+        .wr_data    (tmp_decode2alloc),
+        .rd_en_cnt  (rename_en_cnt),
+        .rd_data    (rename_in),
+
+        .free_scnt  (alloc_rdy_scnt),
+        .used_scnt  (alloc_vld_scnt)
+    );
+
+    /* >> ==== 2. Rename Stage ==== >> */
+
+    logic [`N-1:0] rnme_is_brch;
+    logic [`N:0][$clog2(`N):0] rnme_snap_prefix_cnt;
+    logic [$clog2(`N):0] rnme_snap_lim_cnt;
+    compactor #(
+        .WIDTH(`N)
+    ) comp_rnme_snap (
+        .req        (rnme_is_brch),
+        .rdy        (bman_in.snap_rdy_scnt),
+        .gnt_cnt    (rnme_snap_lim_cnt),
+        .prefix_cnt (rnme_snap_prefix_cnt)
+    );
+
+    always_comb begin
+        foreach (rnme_is_brch[n])
+            rnme_is_brch[n] = rename_in[n].fu_idx == FU_BRU;
+
+        rename_en_cnt = alloc_vld_scnt;
+        rename_en_cnt = `MIN(rename_rdy_scnt, rename_en_cnt);
+        rename_en_cnt = `MIN(rnme_snap_lim_cnt, rename_en_cnt);
+        bman_out.snap_en_cnt = rnme_snap_prefix_cnt[rename_en_cnt];
+    end
+
+    // handle map table output 
+    always_comb begin
+        map_out = '0;
+        map_out.en_cnt  = rename_en_cnt;
+
+        for (int i = 0; i < rename_en_cnt; i++) begin
+            //handling dest register
+            map_out.ts[i]       = rename_in[i].t;
+            map_out.dsts[i]     = rename_in[i].has_dst
+                ? rename_in[i].inst.r.rd
+                : `ZERO_REG;
+            //handling src tags
+            map_out.src1s[i]    = rename_in[i].inst.r.rs1;
+            map_out.src2s[i]    = rename_in[i].inst.r.rs2;
+        end
+    end
+
+    RENAME_COMMIT_PKT [`N-1:0] tmp_alloc2rename;
+    BMASK             [`N-1:0] tmp_alloc2rename_bmask;
+    always_comb begin
+        logic [`N-1:0] rd_src1s;
+        logic [`N-1:0] rd_src2s;
+
+        tmp_alloc2rename = '0;
+        for (int i = 0; i < `N; ++i) begin
+            // logic [$bits(RENAME_COMMIT_PKT)-$bits(ALLOC_RENAME_PKT)-1:0] diff;
+            // diff = '0;
+            // tmp_alloc2rename[i] = RENAME_COMMIT_PKT'({rename_in[i], diff});
+            tmp_alloc2rename[i] = '{
+                // from ID_RESULT
+`ifdef DEBUG
+                id          : rename_in[i].id,
+`endif
+                PC          : rename_in[i].PC,
+                inst        : rename_in[i].inst,
+                fu_idx      : rename_in[i].fu_idx,
+
+                alu_func    : rename_in[i].alu_func,
+                opa_select  : rename_in[i].opa_select,
+                opb_select  : rename_in[i].opb_select,
+
+                has_dst     : rename_in[i].has_dst,
+                cond_branch : rename_in[i].cond_branch,
+                halt        : rename_in[i].halt,
+                illegal     : rename_in[i].illegal,
+                csr_op      : rename_in[i].csr_op,
+                btq_idx     : rename_in[i].btq_idx,
+
+                // alloc
+                t           : rename_in[i].t,
                 // rename
                 b1hot       : '0,
                 t_old       : '0,
@@ -167,11 +229,11 @@ module dispatch #(parameter
             tmp_alloc2rename_bmask[i] = bman_in.bmask_n[rnme_snap_prefix_cnt[i]];
 
             // actually need src tags?
-            rd_src1s[i] = d_in.d_dat[i].opa_select == OPA_IS_RS1
-                || d_in.d_dat[i].cond_branch;
-            rd_src2s[i] = d_in.d_dat[i].opb_select == OPB_IS_RS2
-                || d_in.d_dat[i].cond_branch
-                || d_in.d_dat[i].fu_idx == FU_STR;
+            rd_src1s[i] = rename_in[i].opa_select == OPA_IS_RS1
+                || rename_in[i].cond_branch;
+            rd_src2s[i] = rename_in[i].opb_select == OPB_IS_RS2
+                || rename_in[i].cond_branch
+                || rename_in[i].fu_idx == FU_STR;
             tmp_alloc2rename[i].t1_rdy  = !rd_src1s[i];
             tmp_alloc2rename[i].t2_rdy  = !rd_src2s[i];
         end
@@ -180,13 +242,13 @@ module dispatch #(parameter
             rnme_snap_out.snap_en[i] = rnme_is_brch[i] && (i < rename_en_cnt);
             rnme_snap_out.b1hot_n[i] = bman_in.b1hot_n[rnme_snap_prefix_cnt[i]]; // only valid if snap_en
 
-            rnme_snap_out.fl_head[i] = free_in.fl_heads_n[free_prefix_cnt[i]];
+            rnme_snap_out.fl_head[i] = rename_in[i].fl_head_snap;
 `ifdef DEBUG
-            rnme_snap_out.btq_idx[i] = d_in.d_dat[i].btq_idx;
+            rnme_snap_out.btq_idx[i] = rename_in[i].btq_idx;
 `endif
-            rnme_snap_out.btq_tail[i]= d_in.d_dat[i].btq_idx + 1 >= `BTQ_SZ ?
+            rnme_snap_out.btq_tail[i]= rename_in[i].btq_idx + 1 >= `BTQ_SZ ?
                 0 :
-                d_in.d_dat[i].btq_idx + 1;
+                rename_in[i].btq_idx + 1;
         end
     end
 
