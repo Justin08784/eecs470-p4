d6ce69896cbff84fc59e3da606e1a064dfbdbc46
Justin Shin <shine777justin@gmail.com>
2025-06-05 10:53:37 -0400

    uftb: made fully assoc. slightly restructured types

--- verilog/uftb.sv
+++ verilog/uftb.sv
@@ -1,8 +1,11 @@
 `include "sys_defs.svh"
 
+localparam FTQ_SZ = 32;
+
 typedef struct packed {
     // fallthrough npc (i.e. npc if no branch taken)
     logic [3:0] end_off;    // offset of last insn in the FB. ft_npc = base + end_off + 1
+    // TODO: use ft_lo4, ft_cry scheme?
 
     // two branch slots: [0, 1]
     struct packed {
@@ -22,7 +25,7 @@ typedef struct packed {
 } FTB_ENTRY;
 
 typedef struct packed {
-    WADDR base;         // base address of FB
+    WADDR       base;   // base address of FB
 
     // pared down FTB entry
     logic       ft;     // fallthrough? else took a branch
@@ -56,15 +59,27 @@ typedef struct packed {
     logic   pred_bim;
     logic   pred_gshare;
 
-    logic   [3:0] off; // offset in fb
-    logic   [$clog2(`FTQ_SZ)-1:0] ftq_idx; // pointer to owning FTQ entry
+    logic   [3:0] off; // offset in fb (if taken, equals offset in FTQ_ENTRY)
+    logic   [$clog2(FTQ_SZ)-1:0] ftq_idx; // pointer to owning FTQ entry
         /* Since multiple contiguous BTQ entries may be associated with an FTQ entry,
         an FTQ entry cannot dequeue until the "last" in the BTQ entry span is reached. */
 } _BTQ_ENTRY;
 
-module ftb #(
-    parameter NUM_LINES=1024,
-    parameter ASSOC = 4
+typedef struct packed {
+    WADDR       base;
+    logic [3:0] pc_off; // pc = base + pc_off
+    WADDR       tgt;
+
+    logic       cond;
+    logic       always_take; // i.e. a cond branch that is always taken?
+    logic       call;
+    logic       ret;
+    logic       jalr;
+} FTB_UPD_PKT;
+
+module uftb #(
+    parameter NUM_LINES=16
+    // fully associative
 ) (
     input clock,
     input reset,
@@ -76,54 +91,43 @@ module ftb #(
     output  FTB_ENTRY   o_tgt,
 
     // puq updates
-    input   struct packed {
-        logic       en;
-        WADDR       base;
-        FTB_ENTRY   fb;
-    } i_upd
+    input   logic       i_upd_en,
+    input   FTB_UPD_PKT i_upd_dat
 );
-    localparam NUM_SETS     = NUM_LINES / ASSOC;
-    localparam SID_BITS     = $clog2(NUM_SETS);
     localparam TAG_SKIMP    = 3;
-    /*
-    TAG_SKIMP = how many bits to drop from the full tag that is required to
-    eliminate aliases (0 for no alias).
+        /* TAG_SKIMP = how many bits to drop from the full tag that is required to
+        eliminate aliases (0 for no alias).
 
-    Increasing will result in more aliases, but acceptable for
-    BTB since they are speculative. Can be worth to save area and logic.
-    */
-    localparam TAG_BITS     = $bits(WADDR) - SID_BITS - TAG_SKIMP;
-    typedef logic [SID_BITS-1:0] SID;
-    typedef logic [TAG_BITS-1:0] TAG;
-    typedef logic [$clog2(ASSOC)-1:0]   WAY;
-    typedef logic [ASSOC-1:0][ASSOC-1:0]AGE;
+        Increasing will result in more aliases, but acceptable for
+        BTB since they are speculative. Can be worth to save area and logic. */
+    localparam TAG_BITS = $bits(WADDR) - TAG_SKIMP;
+    typedef logic [TAG_BITS-1:0]                TAG;
+    typedef logic [$clog2(NUM_LINES)-1:0]       WAY;
+    typedef logic [NUM_LINES-1:0][NUM_LINES-1:0]AGE;
 
     function automatic TAG get_tag(input WADDR waddr);
-        return waddr[SID_BITS+TAG_BITS-1:SID_BITS];
-    endfunction
-    function automatic SID get_sid(input WADDR waddr);
-        return waddr[SID_BITS-1:0];
+        return waddr[TAG_BITS-1:0];
     endfunction
 
     typedef struct packed {
-        logic   [NUM_SETS-1:0][ASSOC-1:0] vld;
-        TAG     [NUM_SETS-1:0][ASSOC-1:0] tag;
-        /* TODO: implement pLRU (tree or bit) for assoc ≥ 8.
-        Maybe auto-switch between LRU and pRLU according to assoc
-        parameter, keeping LRU for assoc < 8. 
-        
-        TODO: We can save bits in the LRU age matrix by storing the upper-triangle
-        bits (top right above diagonal). */
-        AGE     [NUM_SETS-1:0] age;
+        logic   [NUM_LINES-1:0] vld;
+        logic   [NUM_LINES-1:0] dirty; // TODO: unused. use when doing multi-level FTB
+        TAG     [NUM_LINES-1:0] tag;
+        AGE     age;
+            /* TODO:
+            1. implement pLRU (tree or bit) for assoc ≥ 8.
+            Maybe auto-switch between LRU and pRLU according to assoc
+            parameter, keeping LRU for assoc < 8. 
+            2. We can save bits in the LRU age matrix by storing the upper-triangle
+            bits (top right above diagonal). */
     } HEADER;
     HEADER hdr, hdr_n;
-    FTB_ENTRY [NUM_SETS-1:0][ASSOC-1:0] tgt, tgt_n;
+    FTB_ENTRY [NUM_LINES-1:0] tgt, tgt_n;
 
     typedef struct packed {
         logic   hit;
         TAG     tag;
         WAY     way;
-        SID     sid;
     } LOC;
     function automatic LOC locate(
         input HEADER    hdr,
@@ -132,14 +136,12 @@ module ftb #(
         logic   hit;
         TAG     tag;
         WAY     way;
-        SID     sid;
         tag = get_tag(waddr);
-        sid = get_sid(waddr);
 
         way = 0;
         hit = 0;
-        for (int w = 0; w < ASSOC; ++w) begin
-            if (hdr.vld[sid][w] && (tag == hdr.tag[sid][w])) begin
+        for (int w = 0; w < NUM_LINES; ++w) begin
+            if (hdr.vld[w] && (tag == hdr.tag[w])) begin
                 hit = 1;
                 way = w;
                 break;
@@ -149,8 +151,7 @@ module ftb #(
         return '{
             hit : hit,
             tag : tag,
-            way : way,
-            sid : sid
+            way : way
         };
     endfunction
 
@@ -169,122 +170,76 @@ module ftb #(
         return rv;
     endfunction
 
-    // s1: tag access
-    // s2: data access
-    struct packed {
-        logic   hit;
-        SID     sid;
-        WAY     way;
-    } s1r, s1r_n;
-
-    struct packed {
-        logic   en;
-        SID     sid;
-        TAG     tag;
-        WAY     way;
-        FTB_ENTRY dat;
-    } s1w, s1w_n;
-
     // fetch
     always_comb begin
-        // s1
         LOC loc;
         loc = locate(hdr, i_qry);
 
-        s1r_n = '{
-            hit : loc.hit,
-            sid : loc.sid,
-            way : loc.way
-        };
-
-        // s2
-        o_vld = s1r.hit;
-        o_tgt = tgt[s1r.sid][s1r.way];
+        o_vld = loc.hit;
+        o_tgt = tgt[loc.way];
     end
 
     // retire
     always_comb begin
-        // s1
-        logic [NUM_SETS-1:0][ASSOC-1:0] lru;
-        logic [NUM_SETS-1:0][$clog2(ASSOC)-1:0] ways;
+        logic [NUM_LINES-1:0] lru;
         LOC loc;
+        WAY way;
 
-        foreach (lru[s, w])
-            lru[s][w] = &hdr.age[s][w];
+        foreach (lru[w])
+            lru[w] = &hdr.age[w];
 
-        ways = '0;
-        foreach (ways[s]) begin
-            for (int w = 0; w < ASSOC; ++w) begin
-                if (lru[s][w])
-                    ways[s] = w;
-            end
+        way = '0;
+        for (int w = 0; w < NUM_LINES; ++w) begin
+            if (lru[w])
+                way = w;
         end
         
-        loc = locate(hdr, i_upd.base);
-
-        s1w_n = '{
-            en  : i_upd.en,
-            sid : loc.sid,
-            tag : loc.tag,
-            way : ways[loc.sid],
-            dat : i_upd.fb
-        };
-
-        // s2
+        loc = locate(hdr, i_upd_dat.base);
         hdr_n = hdr;
         tgt_n = tgt;
 
-        // if (i_upd.en && !loc.hit) begin // dedup (dont insert if already there)
-        // ^^ This dedup rule should no longer be valid if must be able to selectively
-        // overwrite parts of existing FTB entries.
-        if (s1w.en) begin
-            hdr_n.vld[s1w.sid][s1w.way] = 1;
-            hdr_n.tag[s1w.sid][s1w.way] = s1w.tag;
-            hdr_n.age[s1w.sid]          = update_lru(hdr.age[s1w.sid], s1w.way);
-            /* TODO: since every branch queries the FTB (but not every branch
-            generates an FTB update) we need an LRU update for reads as well,
-            not just writes. */
-            tgt_n[s1w.sid][s1w.way]     = i_upd.fb;
+        if (i_upd_en) begin
+            hdr_n.vld[way]  = 1;
+            hdr_n.tag[way]  = loc.tag;
+            hdr_n.age       = update_lru(hdr.age, way);
+                /* TODO: since every branch queries the FTB (but not every branch
+                generates an FTB update) we need an LRU update for reads as well,
+                not just writes. */
+            tgt_n[way]      = i_upd_dat;
         end
     end
 
     always_ff @(posedge clock) begin
         if (reset) begin
             hdr <= '{
-                vld : '0,
-                tag : '0,
-                age : '1 // *IMPORTANT* empty lines are treated as "oldest"
+                vld     : '0,
+                dirty   : '0,
+                tag     : '0,
+                age     : '1 // *IMPORTANT* empty lines are treated as "oldest"
             };
             tgt <= '0;
-            s1r <= '0;
-            s1w <= '0;
+
         end else begin
             hdr <= hdr_n;
             tgt <= tgt_n;
-            s1r <= s1r_n;
-            s1w <= s1w_n;
+
         end
     end
 
 
-`ifdef DEBUG
+// `ifdef DEBUG
     task automatic print_ftb();
-        for (int s = 0; s < NUM_SETS; ++s) begin
-            if (!(|hdr.vld[s]))
+        for (int w = 0; w < NUM_LINES; ++w) begin
+            if (!hdr.vld[w]) begin
+                $display("  %1d:", w);
                 continue;
-            $display("set[%2d]:", s);
-            for (int w = 0; w < ASSOC; ++w) begin
-                if (!hdr.vld[s][w]) begin
-                    $display("  %1d:", w);
-                    continue;
-                end
-                $display("  %1d: {tag: 0x%x tgt: %x} ",
-                    w,
-                    hdr.tag[s][w],
-                    w2addr(tgt[s][w])
-                );
             end
+            $display("  %1d: {tag: 0x%x tgt: %x} ",
+                w,
+                hdr.tag[w],
+                w2addr(tgt[w])
+            );
         end
     endtask
-`endif
+// `endif
 endmodule
