1e1bccbc8afdf42904dc9fbcd0491482d8875e38
Justin Shin <shine777justin@gmail.com>
2025-06-22 04:36:17 -0400

    cleanup: new subheaders: timescale, util_macros, config

---
+++ include/config.svh
@@ -0,0 +1,62 @@
+`ifndef CONFIG_SVH
+`define CONFIG_SVH
+
+///////////////////////////////////
+// ---- Starting Parameters ---- //
+///////////////////////////////////
+
+// some starting parameters that you should set
+// this is *your* processor, you decide these values (try analyzing which is best!)
+
+// superscalar width
+`define N 2
+`define CDB_SZ `N // This MUST match your superscalar width
+
+// sizes
+`define ROB_SZ 64
+`define BTQ_SZ 16
+`define RAS_SZ 16
+    /* BTQ_SZ doubled (form 8). This improved CPI on tight loop
+    programs like branchy.s and branchy_nested.s */
+`define PHYS_REG_SZ_P6 32
+`define PHYS_REG_SZ_R10K (32 + `ROB_SZ)
+
+// worry about these later
+`define BRANCH_PRED_SZ xx
+`define LSQ_SZ 12
+`define SQ_RET_BUF_SZ 4
+parameter GHR_BUF_SZ= 32;
+parameter GHR_LEN   = 8;
+parameter BMASK_LEN = 8; // i.e. number of branch checkpoints
+typedef logic [BMASK_LEN-1:0] BMASK;
+
+// functional units (you should decide if you want more or fewer types of FUs)
+`define NUM_FU_ALU 2
+`define NUM_FU_MUL 1
+`define NUM_FU_LOD 1
+`define LD_BAY_SZ 2 //num load bays in the FU
+`define NUM_FU_STR 1
+`define NUM_FU_BRU 1
+`define NUM_FU_TOTAL `NUM_FU_ALU + `NUM_FU_MUL + `NUM_FU_LOD + `NUM_FU_STR + `NUM_FU_BRU
+
+// number of mult stages (2, 4) (you likely don't need 8)
+`define MUL_STAGES 16
+// Justin: funny enough we need at least 8 or else multiply is on critical path
+
+///////////////////////////////
+// --- Compil. Controls ---- //
+///////////////////////////////
+/* How can we implement this in the Makefile? */
+// comment out to enable synth only constructions
+// `define SYNTH
+
+`ifndef SYNTH
+// comment out to disable DEBUG:
+// `define DEBUG
+// comment to disable clock cycle print
+// `define CYCLE_PRINT
+// comment out to...
+// `define PC_GEN_TEST_MODE
+`endif
+
+`endif // CONFIG_SVH
--- include/sys_defs.svh
+++ include/sys_defs.svh
@@ -10,79 +10,9 @@
 `ifndef __SYS_DEFS_SVH__
 `define __SYS_DEFS_SVH__
 
-// all files should `include "sys_defs.svh" to at least define the timescale
-`timescale 1ns/100ps
-
-// helpful macros
-`define MIN(a, b) ((a) < (b) ? (a) : (b))
-`define MAX(a, b) ((a) > (b) ? (a) : (b))
-
-`define CNT_TYPE(max) logic [$clog2(max+1)-1:0] // smallest bit-vector to store max
-`define CNT_SIZE(max) ($clog2(max+1))           // ...and number of bits in that type
-
-`define IDX_TYPE(len) logic [$clog2(len)-1:0]   // smallest bit-vector to index an array of length len
-`define IDX_SIZE(len) ($clog2(len))             // ...and number of bits in that type
-
-`define UCAST_LEN(n, max) ($clog2(max+1)'(unsigned'(n)))
-`define UCAST_FIT(n) (($clog2(n+1))'(unsigned'(n)))    // cast fit unsigned
-
-///////////////////////////////////
-// ---- Starting Parameters ---- //
-///////////////////////////////////
-
-// some starting parameters that you should set
-// this is *your* processor, you decide these values (try analyzing which is best!)
-
-// superscalar width
-`define N 2
-`define CDB_SZ `N // This MUST match your superscalar width
-
-// sizes
-`define ROB_SZ 64
-`define BTQ_SZ 16
-`define RAS_SZ 16
-    /* BTQ_SZ doubled (form 8). This improved CPI on tight loop
-    programs like branchy.s and branchy_nested.s */
-`define PHYS_REG_SZ_P6 32
-`define PHYS_REG_SZ_R10K (32 + `ROB_SZ)
-
-// worry about these later
-`define BRANCH_PRED_SZ xx
-`define LSQ_SZ 12
-`define SQ_RET_BUF_SZ 4
-parameter GHR_BUF_SZ= 32;
-parameter GHR_LEN   = 8;
-parameter BMASK_LEN = 8; // i.e. number of branch checkpoints
-typedef logic [BMASK_LEN-1:0] BMASK;
-
-// functional units (you should decide if you want more or fewer types of FUs)
-`define NUM_FU_ALU 2
-`define NUM_FU_MUL 1
-`define NUM_FU_LOD 1
-`define LD_BAY_SZ 2 //num load bays in the FU
-`define NUM_FU_STR 1
-`define NUM_FU_BRU 1
-`define NUM_FU_TOTAL `NUM_FU_ALU + `NUM_FU_MUL + `NUM_FU_LOD + `NUM_FU_STR + `NUM_FU_BRU
-
-// number of mult stages (2, 4) (you likely don't need 8)
-`define MUL_STAGES 16
-// Justin: funny enough we need at least 8 or else multiply is on critical path
-
-///////////////////////////////
-// --- Compil. Controls ---- //
-///////////////////////////////
-/* How can we implement this in the Makefile? */
-// comment out to enable synth only constructions
-// `define SYNTH
-
-`ifndef SYNTH
-// comment out to disable DEBUG:
-// `define DEBUG
-// comment to disable clock cycle print
-// `define CYCLE_PRINT
-// comment out to...
-// `define PC_GEN_TEST_MODE
-`endif
+`include "timescale.svh"
+`include "util_macros.svh"
+`include "config.svh"
 
 ///////////////////////////////
 // ---- Basic Constants ---- //
